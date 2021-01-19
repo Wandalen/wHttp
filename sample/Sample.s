6e44e10000e94408793b6e253a5cbd02772e5419
Jack Shell <jackshel45@gmail.com>
2021-01-19 15:11:36 +0200

    Fixed to pass tests

--- sample/Sample.s
+++ sample/Sample.s
@@ -4,9 +4,9 @@ let _ = require( 'whttp' );
 /**/
 
 var got = _.http.retrieve
-( {
+({
   uri : 'https://www.google.com/',
   sync : 1,
   attemptLimit : 2,
-} );
+});
 console.log( got.response.body.substring( 0, 60 ) + '...' );
