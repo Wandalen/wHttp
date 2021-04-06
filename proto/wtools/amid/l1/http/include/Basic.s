6b7457d3ff03f9532037b18f46410a59e381625c
wandalen <wandalen.me@gmail.com>
2021-04-06 17:12:24 +0300

    .

--- proto/wtools/amid/l1/http/include/Basic.s
+++ proto/wtools/amid/l1/http/include/Basic.s
@@ -7,7 +7,7 @@
 
 if( typeof module !== 'undefined' )
 {
-  let _ = require( '../../../../../wtools/Tools.s' );
+  let _ = require( '../../../../../node_modules/Tools' );
   _.include( 'wConsequence' );
   module[ 'exports' ] = _global_.wTools;
 }
