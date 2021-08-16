46471850f05bdcb1824b2db3c91680b579cb7c23
dmvict <dm.vict.kr@gmail.com>
2021-08-16 09:31:39 +0300

    Improve test routine `retrieve`, use no `deasync` implicitly

--- proto/wtools/amid/l1.test/Http.test.s
+++ proto/wtools/amid/l1.test/Http.test.s
@@ -24,45 +24,63 @@ const _ = _global_.wTools;
 
 function retrieve( test )
 {
-  test.case = 'single URI';
-  var got = _.http.retrieve
-  ( {
-    uri : 'https://www.google.com/',
-    sync : 1,
-    attemptLimit : 1,
-  } );
-  var exp = '<!doctype html><html itemscope="" itemtype="http://schema.or...';
-  test.identical( got.response.body.substring( 0, 60 ) + '...', exp );
+  const a = test.assetFor( false );
 
-  //
+  /* - */
 
-  test.case = 'array of URI';
-  var uris = [];
-  var results = [];
-  const l = 100;
-  for( let i = 0; i < l; i++ )
+  a.ready.then( () =>
   {
-    uris.push( 'https://www.google.com/' );
-    results.push( '<!doctype html><html itemscope="" itemtype="http://schema.or...' )
-  }
+    test.case = 'single URI';
+    return _.http.retrieve
+    ({
+      uri : 'https://www.google.com/',
+      attemptLimit : 1,
+    });
+  });
+  a.ready.then( ( op ) =>
+  {
+    var exp = '<!doctype html><html itemscope="" itemtype="http://schema.or...';
+    test.identical( op.response.body.substring( 0, 60 ) + '...', exp );
+    return null;
+  });
 
-  var hooksArr = _.http.retrieve
-  ( {
-    uri : uris,
-    sync : 1,
-    attemptLimit : 3
-  } );
-  var got = [];
-  for( let i = 0; i < hooksArr.length; i++ )
-  got[ i ] = hooksArr[ i ].response.body.substring( 0, 60 ) + '...';
+  /* */
 
-  var exp = results;
-  test.identical( got, exp );
+  a.ready.then( () =>
+  {
+    test.case = 'array of URIs';
+    var uris = _.array.make( 100 );
+    for( let i = 0; i < uris.length; i++ )
+    uris[ i ] = 'https://www.google.com/';
+
+    return _.http.retrieve
+    ({
+      uri : uris,
+      attemptLimit : 3
+    });
+  });
+  a.ready.then( ( op ) =>
+  {
+    var got = _.array.make( op.length );
+    for( let i = 0; i < op.length; i++ )
+    got[ i ] = op[ i ].response.body.substring( 0, 60 ) + '...';
+
+    var exp = _.array.make( 100 );
+    for( let i = 0 ; i < exp.length ; i++ )
+    exp[ i ] = '<!doctype html><html itemscope="" itemtype="http://schema.or...';
+
+    test.identical( got, exp );
+    return null;
+  });
+
+  /* - */
+
+  return a.ready;
 }
 retrieve.description =
 `
 Makes GET requests to the given URI
-`
+`;
 
 //
 
